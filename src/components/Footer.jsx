5b93ab194e7e455b916217dd8c0b787302dab52a
Miro Manestar <miroimanestar@gmail.com>
2022-03-01 20:31:41 -0500

    Began modifying about us page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,16 @@
 import React from 'react'
+import {
+    IconButton
+} from '@mui/material'
 import { createUseStyles } from 'react-jss'
+
+import InstagramIcon from '@mui/icons-material/Instagram'
+import FacebookIcon from '@mui/icons-material/Facebook'
+import TwitterIcon from '@mui/icons-material/Twitter'
+import YouTubeIcon from '@mui/icons-material/YouTube'
+import LinkedInIcon from '@mui/icons-material/LinkedIn'
+
+import * as social from '../../content/meta/social.json'
 
 const useStyles = createUseStyles(theme => ({ 
     root: {
@@ -10,15 +21,69 @@ const useStyles = createUseStyles(theme => ({
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
+
+        [theme.breakpoints.down('md')]: {
+            flexWrap: 'wrap'
+        }
+    },
+
+    social: {
+        position: 'absolute',
+        left: '20px',
+
+        '& a': {
+            backgroundColor: theme.colors.primary,
+            color: 'mutedText',
+            marginRight: '10px',
+            transition: '.2s',
+
+            '&:hover': {
+                transform: 'translateY(-1px)',
+                backgroundColor: theme.colors.primary,
+                filter: 'brightness(0.95)',
+                boxShadow: theme.boxShadows.light
+            },
+        },
+
+        [theme.breakpoints.down('md')]: {
+            position: 'relative',
+            paddingBottom: '5px',
+            paddingTop: '10px',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            left: 0,
+        },
+    },
+
+    copyright: {
+        [theme.breakpoints.down('md')]: {
+            width: '100%',
+            textAlign: 'center',
+            marginBottom: '10px',
+        }
     }
 }))
 
 const Footer = () => {
     const classes = useStyles()
-
+    const show_social = true
     return (
         <div className={classes.root}>
-            Copyright &copy; 2022 Illuminate SAU
+            { show_social &&
+                <>
+                <div className={classes.social}>
+                    { social.instagram && <IconButton href={social.instagram}><InstagramIcon /></IconButton> }
+                    { social.facebook && <IconButton href={social.facebook}><FacebookIcon /></IconButton> }
+                    { social.twitter && <IconButton href={social.twitter}><TwitterIcon /></IconButton> }
+                    { social.youtube && <IconButton href={social.youtube}><YouTubeIcon /></IconButton> }
+                    { social.linkedin && <IconButton href={social.linkedin}><LinkedInIcon /></IconButton> }
+                </div>
+                </>
+            }
+            <div className={classes.copyright}>
+                Copyright &copy; 2022 Illuminate SAU
+            </div>
         </div>
     )
 }
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import { createUseStyles } from 'react-jss'
 import {
-    Typography
+    Grid
 } from '@mui/material'
 import Header from '../components/Header'
 import Main from '../components/Main'
+import ContentCard from '../components/ContentCard'
 
 import * as DATA from '../../content/pages/about.json'
-const sections = DATA.sections || []
 
 const useStyles = createUseStyles(theme => ({ 
     container: {
@@ -26,6 +26,44 @@ const useStyles = createUseStyles(theme => ({
         '& div': {
             maxWidth: '75ch',
         }
+    },
+
+    images: {
+        '& img': {
+            height: '200px',
+            borderRadius: '0.75rem',
+            boxShadow: theme.boxShadows.dark
+        }
+    },
+
+    imageContainer: {
+        width: 'fit-content',
+    },
+
+    imgDesc: {
+        textAlign: 'center'
+    },
+
+    responsive: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: '90px 16px',
+
+        [theme.breakpoints.down('md')]: {
+            padding: '45px 16px',
+        },
+    },
+
+    contentCard: {
+        background: theme.colors.darkGradient,
+        color: 'white !important',
+
+        '& h4': {
+            fontSize: '1.5rem',
+            color: theme.colors.mutedText2,
+            padding: theme.spacing(0, 5)
+        }
     }
 }))
 
@@ -36,16 +74,47 @@ const About = () => {
         <div className={classes.root}>
         <Header title={'About'} />
         <Main className={classes.container}>
-            {
-                sections.map((section, index) => 
-                    <section className={classes.section} key={`section_${ index }`}>
-                        <div>
-                            <Typography variant="h3">{section.title}</Typography>
-                            <Typography variant="h5" sx={{ textAlign: section.text_align }}>{section.description}</Typography>
+            <section className={classes.responsive}>
+                <Grid 
+                    container
+                    columns={{ xs: 1, md: 2}}
+                    spacing={{ xs: 8, md: 2 }}
+                    columnSpacing={{ md: 16}}
+                >
+                    <Grid container item
+                        xs={1} md={1}
+                    >
+                        <div className={classes.images}>
+                            {
+                                DATA.images.map((image, index) => (
+                                    <div className={classes.imageContainer}>
+                                        <img 
+                                            key={`about-img-${index}`} 
+                                            src={image.image} 
+                                            alt={image.description}
+                                        />
+                                        <div className={classes.imgDesc}>{image.description}</div>
+                                    </div>
+                                ))
+                            }
                         </div>
-                    </section>
-                )
-            }
+                    </Grid>
+                    <Grid 
+                        container item
+                        className={classes.info}
+                        columns={{ xs: 1, md: 2 }}
+                        xs={1} md={1}
+                    >
+                        <ContentCard
+                            title={DATA.title}
+                            subtitle={DATA.subtitle}
+                            cls={classes.contentCard}
+                        >
+                            {DATA.content}
+                        </ContentCard>
+                    </Grid>
+                </Grid>
+            </section>
         </Main>
         </div>
     )
